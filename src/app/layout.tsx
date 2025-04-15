d6b9327ae48d9f5de8badf1be1c5b7a3336434f0
Jaron De Klein <153074306+CodebyJaron@users.noreply.github.com>
2025-04-15 21:01:04 +0200

    i want 100 100 100 100 on pagespeed!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,8 +75,6 @@ export const viewport: Viewport = {
     themeColor: "#7c3aed",
     width: "device-width",
     initialScale: 1,
-    maximumScale: 1,
-    userScalable: false,
 };
 
 export default function RootLayout({
